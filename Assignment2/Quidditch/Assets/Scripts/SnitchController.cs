d8c63f2b48c0a0b5d5f7ca26045cd6577448540a
jamesperalta <jamesperalta35@gmail.com>
2020-02-25 21:37:27 -0700

    Finished the basic golden snitch on a field with a boundary that isn't working

---
+++ Assignment2/Quidditch/Assets/Scripts/SnitchController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnitchController : MonoBehaviour
+{
+    private CharacterController controller;
+    private float baseSpeed = 10.0f;
+    private float rotSpeedX = 3.0f;
+    private float rotSpeedY = 1.5f;
+
+    public float speed;
+    public Rigidbody rb;
+    float moveHorizontal;
+    float moveVertical;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    //Add logic to make it collide on walls
+    //void OnTriggerExit(Collider other)
+    //{
+    //    this.transform.Translate(Vector3.right * -moveHorizontal);
+    //    this.transform.Translate(Vector3.forward * -moveVertical);
+    //}
+}
