c18c85ec748fe760cd2ddcfd5ffd0ff7f2b31709
jamesperalta <jamesperalta35@gmail.com>
2020-03-06 21:59:00 -0700

    Added Camera Controller and fixed score board on screen

--- Assignment2/Quidditch/Assets/Scripts/CameraController.cs
+++ Assignment2/Quidditch/Assets/Scripts/CameraController.cs
@@ -7,16 +7,33 @@ public class CameraController : MonoBehaviour
     public GameObject player;
 
     private Vector3 offset;
+    private int pos;
+    private Vector3[] cameraAngles = {new Vector3(0, 60, -225), new Vector3(0, 60, 225), new Vector3(225, 60, 0), new Vector3(-225, 60, 0)};
 
     // Start is called before the first frame update
     void Start()
     {
-        //offset = transform.position - player.transform.position;
+        pos = 0;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        //transform.position = player.transform.position + offset;
+        this.gameObject.transform.position = cameraAngles[pos % 4];
+        if (Input.GetKeyDown("space"))
+        {
+            pos += 1;
+        }
+
+        Vector3 target = LocateGoldenSnitch();
+        transform.LookAt(target);
+    }
+
+    // AI Functions
+    Vector3 LocateGoldenSnitch()
+    {
+        var pos = GameObject.Find("Golden-Snitch").transform.position;
+
+        return pos;
     }
 }
